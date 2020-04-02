3169c454902289c5d02408bf186323872a662be7
ravikumar-spark <63037958+ravikumar-spark@users.noreply.github.com>
2020-04-02 12:33:08 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+s = 'learning python is easy'
+n = len(s)
+i = 0
+print('forward direction')
+while i<n:
+    print(s[i])
+    i= i+1
+print('backward direction')
+i = -1
+while i>= -n:
+    print(s[i])
+    i = i-1
